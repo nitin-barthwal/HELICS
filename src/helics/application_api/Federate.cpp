83be70f8fa465f6613f5331901d9574320da7fd4
Trevor Hardy <trevor.hardy@pnnl.gov>
2020-09-03 13:39:45 -0700

    Normalize destination_targets
    Standardized names:
    	destination_targets
    	destinationtargets
    	destinationTargets
    
    Deprecated names:
    	desttargets
    	destTargets

--- src/helics/application_api/Federate.cpp
+++ src/helics/application_api/Federate.cpp
@@ -801,8 +801,9 @@ static void loadOptions(Federate* fed, const Inp& data, Filter& filt)
     addTargets(data, "sourcetargets", asrc);
     addTargets(data, "sourceTargets", asrc);
     addTargets(data, "source_targets", asrc);
-    addTargets(data, "destTargets", adest);
-    addTargets(data, "desttargets", adest);
+    addTargets(data, "destinationTargets", adest);
+    addTargets(data, "destinationtargets", adest);
+    addTargets(data, "destination_targets", adest);
 }
 
 void Federate::registerFilterInterfacesJson(const std::string& jsonString)
